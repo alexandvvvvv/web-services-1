9339de8e2311c9a8cdccc49c7d8ff98467f3868a
Александр Ванцев <alexandervantsev@yandex.ru>
2021-03-16 16:26:36 +0300

    init

---
+++ src/com/company/CoffeeWebService.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+import java.util.List;
+
+@WebService(serviceName = "CoffeeService")
+public class CoffeeWebService {
+    @WebMethod(operationName = "getCoffees")
+    public List<Coffee> getCoffees() {
+        PostgreSQLDAO dao = new PostgreSQLDAO();
+        return dao.getCoffees();
+    }
+    @WebMethod(operationName = "getFilteredCoffees")
+    public List<Coffee> getFilteredCoffees(@WebParam(name="filter")CoffeeFilter filter) {
+        PostgreSQLDAO dao = new PostgreSQLDAO();
+        return dao.getFilteredCoffees(filter);
+    }
+}
